5ddabf229ef16167533e13091ebe714b04101690
abdullaqudrat <aqudrat630@gmail,com>
2018-12-03 14:22:10 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: {
+    main: "./lib/index.js",
+    test: "mocha!./test/index.js"
+  },
+  output: {
+    path: __dirname,
+    filename: "[name].bundle.js"
+  },
+  mode: 'development',
+  module: {
+   rules: [
+     { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+     { test: /\.css$/, loader: "style!css" },
+     { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
+   ]
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.css', '.scss']
+  }
+};
